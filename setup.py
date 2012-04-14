060966bad41377c20ddb3fff3c2ae01bc965c36a
Niall O'Higgins <niallo@unworkable.org>
2012-04-13 22:56:52 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+requires = ['pymongo']
+tests_require = requires + ['nose', 'coverage']
+
+setup(
+    name='strider-python-mongodb-test',
+    version='1.0',
+    author="Niall O'Higgins",
+    author_email='niallo@beyondfog.com',
+    description='Useful towel-related stuff.',
+    long_description=open('README.txt').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=tests_require,
+    test_suite="nose.collector",
+)
